04af7c1d2e4ee9447a09d156a3c5bcef317c11ad
matteo-panelli <92318790+matteo-panelli@users.noreply.github.com>
2024-04-26 18:34:40 +0200

    fine

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'tua_chiave_segreta'  
+app.secret_key = 'tua_chiave_segreta'
 DATABASE = 'mma.db'
 app.config['UPLOAD_FOLDER'] = './static/uploads'
 
+# Funzione per verificare se il tipo di file è consentito
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
 
+# Ottiene un collegamento al database, creando una connessione se non esiste
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -19,13 +21,14 @@ def get_db():
         db.row_factory = sqlite3.Row
     return db
 
+# Chiude la connessione al database alla fine della richiesta/applicazione
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
-
+# Pagina principale che mostra tutti i post
 @app.route('/')
 def index():
     conn = get_db()
@@ -37,8 +40,7 @@ def index():
     ''').fetchall()
     return render_template('index.html', posts=posts)
 
-
-
+# Mostra un post specifico e i suoi commenti
 @app.route('/post/<int:post_id>')
 def post(post_id):
     conn = get_db()
@@ -46,15 +48,12 @@ def post(post_id):
     comments = conn.execute('SELECT * FROM comments WHERE post_id = ?', (post_id,)).fetchall()
     return render_template('post.html', post=post, comments=comments)
 
-
-
+# Visualizza la pagina della classifica
 @app.route('/classifica')
 def news():
-    
     return render_template('classifica.html')
 
-
-
+# Permette l'aggiunta di un nuovo post
 @app.route('/add_post', methods=['GET', 'POST'])
 def add_post():
     if not session.get('logged_in'):
@@ -68,12 +67,10 @@ def add_post():
         image_path_db = None
 
         if image and allowed_file(image.filename):
-            
             image_folder = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
             image_filename = title + '_' + image.filename
             image_path = os.path.join(image_folder, image_filename)
             image.save(image_path)
-           
             image_path_db = os.path.join(app.config['UPLOAD_FOLDER'], image_filename)  
 
         try:
@@ -82,23 +79,20 @@ def add_post():
                          (title, content, session['user_id'], image_path_db))
             conn.commit()
         except Exception as e:
-            y
             conn.rollback()
             flash('Errore durante l\'inserimento del post.')
             print("Error occurred:", e)
         finally:
-           
             conn.close()
 
         flash_message = 'Post aggiunto con successo.'
         if image_path_db:
-            flash_message = 'Post con immagine aggiunto con successo.'
+            flash_message += ' con immagine'
         flash(flash_message)
         return redirect(url_for('index'))
     return render_template('add_post.html')
 
-
-
+# Permette di aggiungere un commento a un post
 @app.route('/add_comment/<int:post_id>', methods=['POST'])
 def add_comment(post_id):
     if not session.get('logged_in'):
@@ -123,39 +117,31 @@ def add_comment(post_id):
 
     return redirect(url_for('post', post_id=post_id))
 
-
-
-@app.route('/edit_post/<int:post_id>', methods=['GET'])
-def edit_post_form(post_id):
-    if 'user_id' in session:
-        conn = get_db()
-        post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
-        if post and post['user_id'] == session['user_id']:
-            return render_template('edit_post.html', post=post)
-        else:
-            flash('You do not have permission to edit this post.')
-    else:
-        flash('Please log in to edit posts.')
-    return redirect(url_for('index'))
-
-
-@app.route('/edit_post/<int:post_id>', methods=['POST'])
+# Permette di modificare un post esistente
+@app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
 def edit_post(post_id):
-    if 'user_id' in session and request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        conn = get_db()
-        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
-        conn.commit()
-        flash('Post updated successfully.')
-        return redirect(url_for('post', post_id=post_id))
-    else:
-        flash('Invalid request or not logged in.')
+    if 'user_id' not in session:
+        flash('Effettua l\'accesso per modificare il post.')
+        return redirect(url_for('login'))
+
+    conn = get_db()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    
+    if not post or post['user_id'] != session['user_id']:
+        flash('Non hai i permessi per modificare questo post o il post non esiste.')
         return redirect(url_for('index'))
 
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+        conn.commit()
+        flash('Post aggiornato con successo.')
+        return redirect(url_for('index'))  # Redirect to index after successful update
 
+    return render_template('edit_post.html', post=post)
 
-
+# Permette di eliminare un post
 @app.route('/delete_post/<int:post_id>', methods=['POST'])
 def delete_post(post_id):
     if 'user_id' in session:
@@ -164,23 +150,21 @@ def delete_post(post_id):
         if user:
             post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
             if post:
-                
                 if user['ruolo'] == 'admin' or post['user_id'] == session['user_id']:
                     conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
                     conn.commit()
-                    flash('Post deleted successfully.')
+                    flash('Post eliminato con successo.')
                 else:
-                    flash('You do not have permission to delete this post.')
+                    flash('Non hai i permessi per eliminare questo post.')
             else:
-                flash('Post not found.')
+                flash('Post non trovato.')
         else:
-            flash('User not found.')
+            flash('Utente non trovato.')
     else:
-        flash('Please log in to delete posts.')
+        flash('Effettua il login per eliminare i post.')
     return redirect(url_for('index'))
 
-
-
+# Permette di eliminare un commento
 @app.route('/delete_comment/<int:post_id>/<int:comment_id>', methods=['POST'])
 def delete_comment(post_id, comment_id):
     if 'user_id' in session:
@@ -188,7 +172,6 @@ def delete_comment(post_id, comment_id):
         user_role = conn.execute('SELECT ruolo FROM users WHERE id = ?', (session['user_id'],)).fetchone()
         comment = conn.execute('SELECT * FROM comments WHERE id = ? AND post_id = ?', (comment_id, post_id)).fetchone()
         if comment:
-           
             if user_role['ruolo'] == 'admin' or comment['user_id'] == session['user_id']:
                 conn.execute('DELETE FROM comments WHERE id = ? AND post_id = ?', (comment_id, post_id))
                 conn.commit()
@@ -202,11 +185,7 @@ def delete_comment(post_id, comment_id):
         flash('Effettua il login per eliminare i commenti.')
     return redirect(url_for('post', post_id=post_id))
 
-
-
-
-
-
+# Gestisce il login degli utenti
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -221,8 +200,8 @@ def login():
             session['logged_in'] = True
             session['user_id'] = user['id']
             session['username'] = username
-            
-            
+
+            # Verifica se l'utente è l'amministratore
             if username == 'admin' and password == 'admin':
                 session['role'] = 'admin'
                 
@@ -232,8 +211,7 @@ def login():
 
     return render_template('login.html')
 
-
-
+# Gestisce la registrazione degli utenti
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -253,7 +231,6 @@ def register():
             conn.close()
         return redirect(url_for('login'))
     return render_template('register.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
